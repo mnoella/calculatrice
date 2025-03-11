f0e8b40e5af51271c584ccb49f87340043b50268
mnoella <mailholnoella@gmail.com>
2025-03-11 09:49:53 +0100

    feat(test unitaire subtract, multiply, history)

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,6 +1,29 @@
 export class Calculator {
+    private history: { operation: string; operands: number[]; res: number }[] = [];
+
     add(a: number, b: number): number {
         const res = a + b;
+        this.history.push({ operation: "add", operands: [a,b], res });
         return res;
+    }
+
+    subtract(a: number, b: number): number {
+        const res = a - b;
+        this.history.push({ operation: "subtract", operands: [a,b], res });
+        return res;
+    }
+
+    multiply(a: number, b: number): number {
+        const res = a * b;
+        this.history.push({ operation: "multiply", operands: [a,b], res });
+        return res;
+    }
+
+    getHistory(){
+        return this.history;
+    }
+
+    clearHistory(){
+        this.history = [];
     }
 }
--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -10,4 +10,29 @@ describe("Calculator", () => {
     it("should add two numbers corretcly", () => {
         expect(calculator.add(2,3)).toBe(5);
     });
-})
+
+    it("should subtract two numbers corretcly", () => {
+        expect(calculator.subtract(5,3)).toBe(2);
+    });
+
+    it("should multiply two numbers corretcly", () => {
+        expect(calculator.multiply(4,3)).toBe(12);
+    });
+
+    it("should store history of calculations ", () => {
+        calculator.add(2,2);
+        calculator.subtract(6,3);
+        calculator.multiply(3,3);
+        expect (calculator.getHistory()).toEqual([
+            {operation: "add", operands: [2,2], res: 4},
+            {operation: "subtract", operands: [6,3], res: 3},
+            {operation: "multiply", operands: [3,3], res: 9},
+        ]);
+    });
+
+    it("should clear history", () => {
+        calculator.add(1,1);
+        calculator.clearHistory();
+        expect(calculator.getHistory()).toEqual([]);
+    });
+});
